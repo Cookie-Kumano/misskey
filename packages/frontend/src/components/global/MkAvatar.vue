beabe1c482be639628c767cfe097e607b7939023
Cookie-Kumano <cookie@kumano.moe>
2025-11-11 00:09:34 +0900

    角丸半径を調整

--- packages/frontend/src/components/global/MkAvatar.vue
+++ packages/frontend/src/components/global/MkAvatar.vue
@@ -158,7 +158,7 @@ watch(() => props.user.avatarBlurhash, () => {
 	display: inline-block;
 	vertical-align: bottom;
 	flex-shrink: 0;
-	border-radius: 100%;
+	border-radius: var(--MI-radius);
 	line-height: 16px;
 }
 
@@ -168,7 +168,7 @@ watch(() => props.user.avatarBlurhash, () => {
 	left: 0;
 	right: 0;
 	top: 0;
-	border-radius: 100%;
+	border-radius: var(--MI-radius);
 	z-index: 1;
 	overflow: clip;
 	object-fit: cover;
